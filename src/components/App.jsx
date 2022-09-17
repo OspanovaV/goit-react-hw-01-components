bec682e74ae74b7ac368788d7f98533f2b7916ca
OspanovaVita <vitaospanova@ukr.net>
2022-09-17 23:23:04 +0300

    add transaction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,12 @@ import { Container } from 'components/Container/Container';
 import { Profile } from 'components/Profile/Profile';
 import { Statistic } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
+import {TransactionHistory} from 'components/TransactionHistory/TransactionHistory'
 
 import user from 'data/user.json';
 import statisticalData from 'data/data.json';
 import friends from 'data/friends.json';
+import transactions from 'data/transactions.json'; 
 
 export const App = () => { 
   return (
@@ -20,6 +22,7 @@ export const App = () => {
       <Statistic title="Upload stats" stats={statisticalData} />
       {/* <Statistic stats={statisticalData} /> */}
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </Container>
   );
 };
